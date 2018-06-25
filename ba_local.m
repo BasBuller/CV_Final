16aa15d88ddba12781f5e12b4b82f7272efb834d
Bas Buller <bas.buller@gmail.com>
2018-06-25 23:24:16 +0200

    bedtime

--- ba_local.m
+++ ba_local.m
@@ -1,3 +1,13 @@
-function cost = ba_local(X0)
+function cost = ba_local(X0, model)
+    load triple_models triple_models
 
+    key_pts = triple_models{model, 4};
+    
+    % Split inputs
+    M = X0(:, 6);
+    S = X0(:, 7:end);
+    
+    % Cost 
+    error = key_pts - M*S;
+    cost = sum(sum(sqrt([error(1,:).^2 + error(2,:).^2; error(3,:).^2 + error(4,:).^2; error(5,:).^2 + error(6,:).^2])));
 end
