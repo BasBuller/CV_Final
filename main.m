7b12eba72a1bea19f146b2320d12f63d9b3ff59e
BasBuller <bas.buller@gmail.com>
2018-06-28 08:12:25 +0200

    nothing much

--- main.m
+++ main.m
@@ -32,14 +32,14 @@ step2               = 0; % Perform feature matching
 step2_vlmatch       = 0; % Perform feature matching using vl_ubcmatch
 step3               = 0; % Apply normalized 8-point RANSAC to find best matches
 step3_matlab        = 0; % Apply normalized 8-point RANSAC to find best matches using MATLAB algorithm
-step4               = 1; % Determine point view matrix
-step5               = 1; % 3D coordinates for 3 and 4 consecutive images
+step4               = 0; % Determine point view matrix
+step5               = 0; % 3D coordinates for 3 and 4 consecutive images
 step6               = 1; % Perform local bundle adjustment
 step7               = 1; % Procrustes analysis
 step8               = 0; % Global bundle adjustment
 step8b              = 0; % Refined global bundle adjustment
 step9               = 0; % Resolve afine ambiguity
-step10              = 0; % Surface plot of complete model without ba
+step10              = 1; % Surface plot of complete model without ba
 step10b             = 0; % Surface plot of the bundle adjusted model
 
 % example plots
